2a7b231a7eb7a184ef005a93d95470e7e9773185
Shawn Koh <shawn@shawnkoh.sg>
2021-11-04 06:51:13 +0800

    Format code

--- mqtt_stuff.py
+++ mqtt_stuff.py
@@ -2,11 +2,11 @@ import dotenv
 import paho.mqtt.client as mqtt
 from dotenv import dotenv_values
 
-HOST = 'xinming.ddns.net'
-USER = 'guest'
+HOST = "xinming.ddns.net"
+USER = "guest"
 PASSWORD = dotenv_values["SERVER_PASSWORD"]
-PREDICT_TOPIC = 'posture/predict'
-CLASSIFY_TOPIC = 'posture/classify'
+PREDICT_TOPIC = "posture/predict"
+CLASSIFY_TOPIC = "posture/classify"
 
 
 def on_connect(client: mqtt.Client, userdata, flags, result_code):
@@ -14,8 +14,10 @@ def on_connect(client: mqtt.Client, userdata, flags, result_code):
     client.subscribe(TOPIC)
     client.publish("hello/world", "test")
 
+
 def on_message(client: mqtt.Client, userdata, message):
     print(f"Received {message.payload.decode()}")
+
 
 client = mqtt.Client()
 
