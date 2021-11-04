a585d8aa02d517606bdfc0903ff5b3f13c6bca2d
Shawn Koh <shawn@shawnkoh.sg>
2021-11-04 16:32:45 +0800

    Add discover script

--- up_goer/cli.py
+++ up_goer/cli.py
@@ -1,6 +1,20 @@
+import asyncio
+
 import click
+from bleak import BleakScanner
 
 
 @click.group()
 def run():
     pass
+
+
+@run.command()
+def discover():
+    asyncio.run(discover())
+
+
+async def discover():
+    devices = await BleakScanner.discover()
+    for d in devices:
+        print(d)
