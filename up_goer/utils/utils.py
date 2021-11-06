f24f45233a2ff0b3d82c2947b618906448542a0e
Shawn Koh <shawn@shawnkoh.sg>
2021-11-07 03:09:51 +0800

    format files

--- up_goer/utils/utils.py
+++ up_goer/utils/utils.py
@@ -2,8 +2,4 @@ import platform
 
 
 def get_tag_address(mac, core_bluetooth):
-    return (
-        mac
-        if platform.system() != "Darwin"
-        else core_bluetooth
-    )
+    return mac if platform.system() != "Darwin" else core_bluetooth
